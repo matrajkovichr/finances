eb414b56e6ac51fbcdf813a8f78adfffb22fcfca
Matej <matrajkovichr@gmail.com>
2018-03-30 13:23:49 +0200

    Host

--- finances/settings.py
+++ finances/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
@@ -25,7 +24,7 @@ SECRET_KEY = 'jna!8*)cwj9f=7@6w(gm*t3es1pmhxj6ml)-zx-&q$*cw9$tw('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'matrajkovichr.pythonanywhere.com']
 
 
 # Application definition
